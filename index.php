2f2e79506ab8eeda1310f0632f325ad9dac20d45
Jaffar Abbas <gamaportal8@gmail.com>
2022-04-27 19:16:13 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['upload'])) {
+        // $images = $_FILE['images'];
+        // echo $images['name'];
+        // Get the image and convert into string
+        // Encode the image string data into base64
+        $data = base64_encode($img);
+        // Display the output
+        echo $data;
+    }
+}
+?>
+
+<form action="index.php" method="POST" enctype="multipart/form-data">
+    <input type="file" name="images">
+    <button type="submit" class="btnStyle" name="upload"> Upload</button>
+</form>
