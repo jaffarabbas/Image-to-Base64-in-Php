d7ea8e85005626e6743936dc736a31c5a3f97423
Jaffar Abbas <gamaportal8@gmail.com>
2022-05-12 19:10:34 +0500

    Create pass.php

---
+++ pass.php
@@ -0,0 +1,6 @@
+<!-- $2y$10$qKPsD4pLBvA2izIV6BW5zu7s0gmTAX/52DXvwjWENznwAhGoxk/SC -->
+
+<?php 
+$hash = '$2y$10$qKPsD4pLBvA2izIV6BW5zu7s0gmTAX/52DXvwjWENznwAhGoxk/SC';
+print_r(password_verify('JaffarAbbas@1399', $hash));
+?>
